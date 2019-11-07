86c01a35eda4d436f18e2379b52889580f04937e
vicenteyah <vicente.yah.dzul@icloud.com>
2019-11-06 18:24:57 -0600

    config file was added

---
+++ src/config.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports ={
+    port: process.env.PORT || 3000,
+
+    host: 'localhost',
+    user: 'root',
+    password: 'killer100',//your database pass
+    database:'Proyecto'
+}
