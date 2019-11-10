887475fca63c97bdf3ebaadda4aedf5328b77816
vicenteyah <vicente.yah.dzul@icloud.com>
2019-11-09 22:50:31 -0600

    get and post methods were added and configured to add and get data from that table

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -19,4 +19,36 @@ module.exports = function (app){
             res.json(data);
         });
     });
+
+
+    app.get('/users/:idUsu',(req,res)=>{
+        var idUsu = req.params.idUsu;
+        User.getanUser(idUsu,(err,data)=>{
+            res.status(200).json(data);
+        })
+    });
+
+    app.post('/users',(req,res)=>{
+        const userData = {
+            idUsu: null,
+            email: req.body.email,
+            nombreUsu: req.body.nombreUsu,
+            telUsu: req.body.telUsu,
+            password: req.body.password
+        };
+        User.insertUser(userData,(err,data)=>{
+            if (data && data.insertId) {
+                res.json({
+                    success: true,
+                    msg: 'User Added',
+                    data: data
+                })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    msg: 'error'
+                })
+            }
+        })
+    });
 }
